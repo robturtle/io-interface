10a20d39cfa0857bdcc6de94da91b3676c6848b3
Yang Liu <jeremyrobturtle@gmail.com>
2019-11-28 20:37:13 -0800

    first implementation

---
+++ src/index.ts
@@ -0,0 +1,112 @@
+import { Either } from 'fp-ts/lib/Either';
+import * as t from 'io-ts';
+import { runtime } from 'ts-transformer-interface';
+
+export interface Caster<T = any> extends t.Type<T> {}
+
+export interface Casters {
+  [type: string]: Caster;
+}
+
+const TypeC: t.Type<runtime.Type> = t.recursion('TypeC', () =>
+  t.union([t.null, t.string, ReferenceTypeC, ArrayTypeC, ParameterizedTypeC, GenericTypeC]),
+);
+
+const ReferenceTypeC: t.Type<runtime.ReferenceType> = t.type({
+  referenceName: t.string,
+});
+
+const ArrayTypeC: t.Type<runtime.ArrayType> = t.type({
+  arrayElementType: TypeC,
+});
+
+const GenericTypeC: t.Type<runtime.GenericType> = t.type({
+  genericParameterName: t.string,
+  genericParameterType: TypeC,
+});
+
+const ParameterizedTypeC: t.Type<runtime.ParameterizedType> = t.type({
+  selfType: t.string,
+  typeArgumentType: TypeC,
+});
+
+export class Decoder {
+  readonly casters: Casters = {};
+  readonly arrayCasters: Casters = {};
+
+  constructor(schemas: runtime.Schema[]) {
+    schemas.forEach(s => this.registerSchema(s));
+  }
+
+  decode<T>(typeName: string, data: unknown): Either<t.Errors, T> {
+    return this.getCaster<T>(typeName).decode(data);
+  }
+
+  decodeArray<T>(typeName: string, data: unknown): Either<t.Errors, T[]> {
+    return this.getArrayCaster<T>(typeName).decode(data);
+  }
+
+  registerSchema(spec: runtime.Schema) {
+    const name = spec.name;
+    const required = spec.props.filter(p => !p.optional);
+    const optional = spec.props.filter(p => p.optional);
+    if (required.length > 0 && optional.length > 0) {
+      this.casters[name] = t.intersection(
+        [t.type(this.buildCasters(name, required)), t.partial(this.buildCasters(name, optional))],
+        name,
+      );
+    } else if (required.length > 0) {
+      this.casters[name] = t.type(this.buildCasters(name, required), name);
+    } else {
+      this.casters[name] = t.partial(this.buildCasters(name, optional), name);
+    }
+    this.arrayCasters[name] = t.array(this.casters[name]);
+  }
+
+  private getCaster<T>(typeName: string): Caster<T> {
+    if (!(typeName in this.casters)) {
+      throw new Error(`decoder for '${typeName}' not registered`);
+    }
+    return this.casters[typeName];
+  }
+
+  private getArrayCaster<T>(typeName: string): Caster<T[]> {
+    if (!(typeName in this.arrayCasters)) {
+      throw new Error(`decoder for '${typeName}[]' not registered`);
+    }
+    return this.arrayCasters[typeName];
+  }
+
+  private buildCasters(name: string, props: runtime.Property[]): Casters {
+    const casters: Casters = {};
+    props.forEach(p => {
+      let caster;
+      try {
+        caster = this.buildTypeCaster(p.type);
+      } catch (e) {
+        throw new Error(`${name}.${p.name}: ${e.message}`);
+      }
+      casters[p.name] = caster;
+    });
+    return casters;
+  }
+
+  private buildTypeCaster(type: runtime.Type): Caster {
+    if (ArrayTypeC.is(type)) {
+      return t.array(this.buildTypeCaster(type.arrayElementType));
+    }
+    if (ReferenceTypeC.is(type)) {
+      return this.getCaster(type.referenceName);
+    }
+    switch (type) {
+      case 'string':
+        return t.string;
+      case 'number':
+        return t.number;
+      case 'boolean':
+        return t.boolean;
+      default:
+        throw new Error(`illegal decoder type '${JSON.stringify(type)}'`);
+    }
+  }
+}
