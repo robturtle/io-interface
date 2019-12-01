7739f02e45b0a71db7c25994f9db48edcb653e03
Yang Liu <jeremyrobturtle@gmail.com>
2019-12-01 12:42:08 -0800

    update build.sh

--- build.sh
+++ build.sh
@@ -8,4 +8,5 @@ cp ./node_modules/ts-transformer-interface/transformer.js ./transform-interface.
 cp ./node_modules/ts-transformer-interface/runtime-schema.js ./runtime-schema.js
 cp ./node_modules/ts-transformer-decoder-cast/transformer.js ./transform-request.js
 tsc
+git add .
 npx pretty-quick
