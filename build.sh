61fff013a17e7320ec140ea50dc768398f08e843
Yang Liu <jeremyrobturtle@gmail.com>
2019-12-01 10:42:24 -0800

    ship with ts-transformer-decoder-cast

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 
 cp ./node_modules/ts-transformer-interface/transformer.js ./transform-interface.js
 cp ./node_modules/ts-transformer-interface/runtime-schema.js ./runtime-schema.js
+cp ./node_modules/ts-transformer-decoder-cast/transformer.js ./transform-request.js
 tsc
 npx pretty-quick
