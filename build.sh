e84addb9d138a17ec1dab295f19cd12b30693d14
Yang Liu <jeremyrobturtle@gmail.com>
2019-11-30 16:26:59 -0800

    re-export macro function

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@
 cp ./node_modules/ts-transformer-interface/transformer.ts ./transform-interface.ts
 cp ./node_modules/ts-transformer-interface/runtime-schema.ts ./runtime-schema.ts
 tsc
+echo 'export function schema<T extends object>(): runtime.Schema;' >> index.d.ts
--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 import { Either } from 'fp-ts/lib/Either';
 import * as t from 'io-ts';
 import { runtime } from 'ts-transformer-interface';
-export { runtime, schema } from 'ts-transformer-interface';
+export { runtime } from 'ts-transformer-interface';
 export interface Caster<T = any> extends t.Type<T> {}
 export interface Casters {
   [type: string]: Caster;
@@ -17,3 +17,4 @@ export declare class Decoder {
   private buildCasters;
   private buildTypeCaster;
 }
+export function schema<T extends object>(): runtime.Schema;
