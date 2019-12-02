495c9297c68de39b5e54fedc25e788b9db29bb70
Yang Liu <jeremyrobturtle@gmail.com>
2019-12-02 14:56:12 -0800

    fix exports take 7

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# I'm so hxcking confused about the tsconfig, just do this quick hack here
+rm -rf lib/
 
 cp ./node_modules/ts-transformer-interface/transformer.ts ./src/transform-interface.ts
 cp ./node_modules/ts-transformer-interface/runtime-schema.ts ./src/runtime-schema.ts
@@ -7,5 +9,13 @@ cp ./node_modules/ts-transformer-decoder-cast/transformer.ts ./src/transform-req
 cp ./node_modules/ts-transformer-interface/transformer.js ./src/transform-interface.js
 cp ./node_modules/ts-transformer-interface/runtime-schema.js ./src/runtime-schema.js
 cp ./node_modules/ts-transformer-decoder-cast/transformer.js ./src/transform-request.js
+
 tsc
+
+cp ./node_modules/ts-transformer-interface/transformer.ts ./lib/transform-interface.ts
+cp ./node_modules/ts-transformer-interface/runtime-schema.ts ./lib/runtime-schema.ts
+cp ./node_modules/ts-transformer-decoder-cast/transformer.ts ./lib/transform-request.ts
+
+cp lib/index.d.ts src
+
 npx pretty-quick
